fd6be0cc099fc440b7841dfca740d907bedff1a8
Ikonnikov Anton <ika@webcrt.ru>
2019-09-26 11:02:12 +0500

    Rest API

---
+++ rest.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+  "log"
+  "net/http"
+  "encoding/json"
+
+  "github.com/gorilla/mux"
+)
+
+type Book struct {
+  ID      string `json:"id"`
+  Title   string `json:"title"`
+  Author *Author `json:"author"`
+}
+
+type Author struct {
+  Firstname string `json:"firstname"`
+  Lastname  string `json:"lastname"`
+}
+
+var books []Book
+
+func getBooks(w http.ResponseWriter, r *http.Request) {
+  w.Header().Set("Content-Type", "application/json")
+  json.NewEncoder(w).Encode(books)
+}
+
+func getBook(w http.ResponseWriter, r *http.Request) {
+  w.Header().Set("Content-Type", "application/json")
+  params := mux.Vars(r)
+  for _, item := range books {
+    if item.ID == params["id"] {
+      json.NewEncoder(w).Encode(item)
+        return
+      }
+  }
+  json.NewEncoder(w).Encode(&Book{})
+}
+
+func main() {
+  r := mux.NewRouter()
+
+  books = append(books, Book{ID: "1", Title: "Война и Мир", Author: &Author{Firstname: "Лев", Lastname: "Толстой"}})
+  books = append(books, Book{ID: "2", Title: "Преступление и наказание", Author: &Author{Firstname: "Фёдор", Lastname: "Достоевский"}})
+
+  r.HandleFunc("/books", getBooks).Methods("GET")
+  r.HandleFunc("/books/{id}", getBook).Methods("GET")
+
+  log.Fatal(http.ListenAndServe(":8000", r))
+}
