02755eb5099553b2958e8785a557a0731024887e
Ikonnikov Anton <ika@webcrt.ru>
2019-09-26 10:25:31 +0500

    isCommand

--- tbot.go
+++ tbot.go
@@ -2,7 +2,6 @@ package main
 
 import (
   "log"
-  "reflect"
 
   "github.com/davecgh/go-spew/spew"
   "github.com/go-telegram-bot-api/telegram-bot-api"
@@ -41,27 +40,6 @@ func main() {
   }
 }
 
-/*func MessageSend(chatID int64, sendMessage string, param interface {}) tgbotapi.Chattable {
-  switch sendType := param.(type) {
-    default:
-      return sendType(chatID, sendMessage)
-  }
-}*/
-
-func Call(m map[string]interface{}, name string, params ... interface{}) (result []reflect.Value, err error) {
-    f = reflect.ValueOf(m[name])
-    if len(params) != f.Type().NumIn() {
-        err = errors.New("The number of params is not adapted.")
-        return
-    }
-    in := make([]reflect.Value, len(params))
-    for k, param := range params {
-        in[k] = reflect.ValueOf(param)
-    }
-    result = f.Call(in)
-    return
-}
-
 // Отправляем сообщение в телеграм канал
 func OnMessage(bot *tgbotapi.BotAPI, message *tgbotapi.Message) {
 
@@ -75,35 +53,25 @@ func OnMessage(bot *tgbotapi.BotAPI, message *tgbotapi.Message) {
   log.Printf("[%s] %d", userName, chatID)
   spew.Dump(message) // выводим то что пришло (Для отладки!!!)
 
-  if message.Entities != nil {
-    command := message.Text[1:]
+  if message.IsCommand() {
+    command := message.Command()
 
     switch command {
       case "start" :
         sendMessage = "Добро пожаловать, " + message.Chat.FirstName + " " + message.Chat.LastName + "!"
-        msg = Call("tgbotapi.NewMessage", chatID, sendMessage)
+        msg = tgbotapi.NewMessage(chatID, sendMessage)
       case "help" :
         sendMessage = "Список доступных комманд:\n1: /BACK_TO_WORK\n2: /start"
-        msg = Call("tgbotapi.NewMessage", chatID, sendMessage)
+        msg = tgbotapi.NewMessage(chatID, sendMessage)
       case "BACK_TO_WORK" :
         sendMessage = "photo.jpg"
-        //msg = Call(chatID, sendMessage, tgbotapi.NewPhotoUpload)
-        //sendType = "file"
+        msg = tgbotapi.NewPhotoUpload(chatID, sendMessage)
       case "sticker" :
         sendMessage = "CAADAgAD8QIAApzW5wooEroAAWAF9ykWBA"
-        //sendType = "sticker"
+        msg = tgbotapi.NewStickerShare(chatID, sendMessage)
       default :
         sendMessage = command
     }
-
-    /*switch sendType {
-      case "file" :
-        msg = tgbotapi.NewPhotoUpload(chatID, sendMessage)
-      case "sticker" :
-        msg = tgbotapi.NewStickerShare(chatID, sendMessage)
-      default :
-        msg = tgbotapi.NewMessage(chatID, sendMessage)
-    }*/
 
     _, err := bot.Send(msg) // отправляем сообщение
     if err != nil {
